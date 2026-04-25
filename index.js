1b783343f397548bba6f76bf37fd93c0e092ba21
Kristal Pithwa <crystalpithwa@gmail.com>
2026-04-25 17:04:08 +0530

    chore: update dependencies and scripts
    - Added mongoose and nodemon to dependencies in package.json
    - Updated start script to use nodemon for automatic restarts
    - Updated package-lock.json with new dependencies and their versions

--- index.js
+++ index.js
@@ -1,104 +1,130 @@
 const express = require("express");
-const database = require("./MOCK_DATA.json");
-const fs = require("fs");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/restapi")
+  .then(() => console.log("MongoDB connected 🚀"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
+
+// Define a Mongoose schema and model for the user
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    gender: { type: String },
+  },
+  { timestamps: true },
+);
+
+const users = mongoose.model("user", userSchema);
+
+// Set up middleware to parse JSON and URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/user", (req, res) => {
-  res.json(database);
+// get all users
+app.get("/api/user", async (req, res) => {
+  try {
+    const data = await users.find();
+    res.json(data);
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).json({ message: "Error fetching users" });
+  }
 });
 
-app.post("/api/user", (req, res) => {
-  console.log("req?.body =>", req?.body);
-
-  const { first_name, last_name, email, gender, ip_address } = req?.body;
+// create new user
+app.post("/api/user", async (req, res) => {
+  const { first_name, last_name, email, gender } = req?.body;
 
   const newUser = {
-    id: database.length + 1,
     first_name: first_name,
     last_name: last_name,
     email: email,
     gender: gender,
-    ip_address: ip_address,
   };
 
-  database.push(newUser);
-
-  // Write the updated database back to the JSON file
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(database), (err) => {
-    if (err) {
-      console.error("Error writing to file", err);
-      res.status(500).json({ message: "Error creating user" });
-    } else {
-      res.json({
-        message: "User created successfully",
-        user: newUser,
+  try {
+    const result = await users.create(newUser);
+    res.status(201).json({
+      user: result,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already exists",
       });
     }
-  });
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
 });
 
+// get user by Id, update user, delete user
 app
-  .get("/api/user/:id", (req, res) => {
-    const userId = JSON.parse(req?.params?.id);
-    const user = database.find((item) => item.id === userId);
-    res.json(user);
-  })
-  .patch("/api/user/:id", (req, res) => {
-    const userId = JSON.parse(req.params.id);
+  .get("/api/user/:id", async (req, res) => {
+    const { id } = req.params;
 
-    const userIndex = database.findIndex((item) => item.id === userId);
-
-    if (userIndex !== -1) {
-      // Merge old + new data
-      database[userIndex] = {
-        ...database[userIndex],
-        ...req.body,
-      };
-
-      fs.writeFile(
-        "./MOCK_DATA.json",
-        JSON.stringify(database, null, 2),
-        (err) => {
-          if (err) {
-            console.error("Error writing to file", err);
-            return res.status(500).json({ message: "Error updating user" });
-          }
-
-          res.json({
-            message: "User updated successfully",
-            user: database[userIndex],
-          });
-        },
-      );
-    } else {
-      res.status(404).json({ message: "User not found" });
+    try {
+      const result = await users.findById(id);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
     }
   })
-  .delete("/api/user/:id", (req, res) => {
-    const userId = JSON.parse(req?.params?.id);
-    const userIndex = database.findIndex((item) => item.id === userId);
+  .patch("/api/user/:id", async (req, res) => {
+    const { id } = req.params;
 
-    if (userIndex !== -1) {
-      database.splice(userIndex, 1);
-      fs.writeFile("./MOCK_DATA.json", JSON.stringify(database), (err) => {
-        if (err) {
-          console.error("Error writing to file", err);
-          res.status(500).json({ message: "Error deleting user" });
-        } else {
-          res.json({
-            message: "User deleted successfully",
-          });
-        }
+    try {
+      const result = await users.findByIdAndUpdate(id, req.body, {
+        new: true,
       });
-    } else {
-      res.status(404).json({ message: "User not found" });
+      res.status(200).json(result);
+    } catch (error) {
+      console.log("error =>", error);
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  })
+  .delete("/api/user/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+      const result = await users.findByIdAndDelete(id);
+
+      if (result) {
+        res.status(200).json({
+          message: "User deleted successfully",
+        });
+      } else {
+        res.status(404).json({
+          message: "User not found",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: "Error deleting user",
+      });
     }
   });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server port ${port}`);
 });
