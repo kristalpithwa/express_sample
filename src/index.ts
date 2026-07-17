9589514bcbbd846b3e537e0809aca3d28bd0c144
Kristal Pithva <kristal.p@mail.enlivendc.com>
2026-07-17 12:49:30 +0530

    feat: update MongoDB connection string in index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+connectDB(
+  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.3",
+);
+
 app.listen(5000, () => {
   console.log("Server running on 5000");
 });
-
-connectDB("mongodb://localhost:27017/user_management");
 
 export default app;
