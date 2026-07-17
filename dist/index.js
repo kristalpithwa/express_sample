327ece385275fa9f39ba5125ac489731cb191ac1
Kristal Pithva <kristal.p@mail.enlivendc.com>
2026-07-17 12:58:10 +0530

    chore: update development run scripts and refactor database connection string initialization

--- dist/index.js
+++ dist/index.js
@@ -9,8 +9,8 @@ const db_1 = __importDefault(require("./config/db"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+(0, db_1.default)("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.3");
 app.listen(5000, () => {
     console.log("Server running on 5000");
 });
-(0, db_1.default)("mongodb://localhost:27017/user_management");
 exports.default = app;
